65d25680bcf226c0478153d99ba2bd1af92e7738
Teng Sear <tengsear4747@gmail.com>
2025-04-23 19:58:43 +0700

    design welcome

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
 
         $user = User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@gmail.com',
+            'email' => 'admin12@gmail.com',
         ]);
         $role = Role :: create(['name' => 'admin']);
         $user->assignRole($role);
